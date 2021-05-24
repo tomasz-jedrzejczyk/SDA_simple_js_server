b0fb2413247d23d0435826d79c9c9dd47723e066
Tomasz Jędrzejczyk <tomasz.jedrzejczyk.dev@gmail.com>
2021-05-24 15:45:01 +0200

    change port in server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const port = process.env.PORT || 8080;
 
 
 const requestListener = function (req, res) {
@@ -7,4 +8,4 @@ const requestListener = function (req, res) {
 }
 
 const server = http.createServer(requestListener);
-server.listen(8080);
+server.listen(port);
