2991c0dc5aa70d014d88c786f503fbaca37ecc7a
tomasz-jedrzejczyk <tomasz.jedrzejczyk.dev@gmail.com>
2021-09-03 00:02:56 +0200

    change name to Marta

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8080;
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end('Hello, Tomasz!');
+  res.end('Hello, Marta!');
 }
 
 const server = http.createServer(requestListener);
