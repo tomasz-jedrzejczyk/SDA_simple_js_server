a22e61c9b5b1aaeeb006f4f5c7f8da1569b65690
Tomasz Jędrzejczyk <71791302+tomasz-jedrzejczyk@users.noreply.github.com>
2022-03-28 23:36:10 +0200

    test commti

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 8080;
 const requestListener = function (req, res) {
   res.writeHead(200);
   res.end('Testowanie jest super SUPER!');
+  res.end('Testowanie jest super SUPER!');
 }
 
 const server = http.createServer(requestListener);
