08b7cb078ad2749e47b42b54f6ffc202182d34ba
Tomasz Jędrzejczyk <71791302+tomasz-jedrzejczyk@users.noreply.github.com>
2022-04-09 13:45:59 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const http = require('http');
-const port = process.env.PORT || 8080;
+const port = process.env.PORT0;
 
 
 const requestListener = function (req, res) {
   res.writeHead(200);
   res.end('Testowanie jest super SUPER!');
-  res.end('Testowanie jest super SUPER!');
-  res.end('Testowanie jest super SUPER!');
-}
+
 
 const server = http.createServer(requestListener);
 server.listen(port);
